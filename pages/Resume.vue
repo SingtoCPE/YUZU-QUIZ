cab6af71ef0c777a6500fd2218d3411e6179061c
singtoCPE <thitipat_cpe18@hotmail.com>
2020-11-13 17:08:11 +0700

    initial project

---
+++ pages/Resume.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container d-flex justify-center align-center">
+    <v-row class="d-flex justify-center align-center pt-1">
+      <v-col class="d-flex justify-center align-center">
+        <v-card>
+          <img src="@/assets/1.png" alt="" width="450" />
+        </v-card>
+      </v-col>
+      <v-col class="d-flex justify-center align-center">
+        <v-card>
+          <img src="@/assets/2.png" alt="" width="450" />
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
+</template>
