934882dc213080ea7067ad1baf56e42a7961328d
singtoCPE <thitipat_cpe18@hotmail.com>
2020-11-16 01:34:39 +0700

    close project

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,25 @@
 <template>
-  <div>this is dashboard</div>
+  <div class="container d-flex justify-center">
+    <v-card class="" width="750">
+      <h1 class="text-center">สิ่งที่ผมทำตามโจทย์กำหนดไม่ได้</h1>
+      <v-divider />
+      <ul class="my-2">
+        <li>
+          หน้าโชว์ employee ผมทำ เลือกเช็คบล๊อค employee แล้วกดลบไม่ได้ครับ
+        </li>
+        <li>validate ทำได้ไม่ดีพอครับ มันเป็นแปลก ๆ ครับ</li>
+        <li>
+          ตอนแก้ไข ผมทำให้ dropdown โชว์ข้อมูลของ employee
+          ที่เราจะแก้ไขไม่ได้ครับ แต่ค่าดึงมาได้ครับ ถ้าแก้ไขเลย ไม่เลือก list
+          ใน dropdown ข้อมูลจะเป็นข้อมูลเก่าที่ดึงมาครับ
+        </li>
+      </ul>
+      <h3 class="text-center">
+        ** ผ่านหรือไม่ผ่าน ก็ไม่เป็นไรครับ ขอบคุณมาก ๆ ครับ
+        ที่หยิบยื่นโอกาสมาให้ผมครับ **
+      </h3>
+    </v-card>
+  </div>
 </template>
 <script>
 export default {
